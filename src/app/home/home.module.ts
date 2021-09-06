93bcb464d48c7415411f5c43e8aa42f4f817d813
Vitor Caetano <vitorlc95@gmail.com>
2021-09-06 11:27:42 -0300

    Trocando o nome de tab1 para home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,17 +2,17 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab1Page } from './tab1.page';
+import { HomePage } from './home.page';
 
-import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { HomePageRoutingModule } from './home-routing.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    Tab1PageRoutingModule
+    HomePageRoutingModule
   ],
-  declarations: [Tab1Page]
+  declarations: [HomePage]
 })
-export class Tab1PageModule {}
+export class HomePageModule {}
