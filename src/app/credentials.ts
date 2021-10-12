7e847403189366018bf35488aef3d6bb68c54ccc
Vitor Caetano <vitorlc95@gmail.com>
2021-10-12 09:35:44 -0300

    Instalando o firebase

---
+++ src/app/credentials.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD6SuRAEPzE5Mk062liJZEriyOB0BQhIIE",
+  authDomain: "fumec-projeto-mobile.firebaseapp.com",
+  projectId: "fumec-projeto-mobile",
+  storageBucket: "fumec-projeto-mobile.appspot.com",
+  messagingSenderId: "1078797195437",
+  appId: "1:1078797195437:web:cd3d6ea5eacd7d21e50e24",
+  measurementId: "G-VCTN4VH0S1"
+};
