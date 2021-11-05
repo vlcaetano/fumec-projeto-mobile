673a68e33845b1dc99829e5e6c33c67c5fe109f4
Vitor Caetano <vitorlc95@gmail.com>
2021-11-04 22:53:00 -0300

    Adicionando mudança da tela para quando o usuário está logado

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -11,26 +11,33 @@ import { UserService } from './../services/usuarios.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  public email: string;
-  public password: string;
+  public email: string
+  public password: string
+  public loggedUser: Usuario
 
   constructor(private auth: AuthLoginService, private route: Router, private usuarioService: UserService) { }
 
   ngOnInit() {
+    this.loggedUser = null
   }
 
-  acessAccount() {
+  public acessAccount() {
     this.auth.logIn(this.email, this.password).then((response) => {
 
       if (response.user.uid) {
         this.auth.setAuthenticated(true)
 
         this.usuarioService.getByUID(response.user.uid).subscribe((users: Usuario[]) => {
-          const [usuarioLogado] = users
+          const [user] = users
+          this.loggedUser = user
 
           this.route.navigateByUrl('/tabs/home');
         })
       }
     })
+  }
+
+  public logout() {
+    //TODO
   }
 }
