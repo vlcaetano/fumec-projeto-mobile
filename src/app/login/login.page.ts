e7417614bbf4adba2ad8b0d1f68b63dde116fd48
Nikolas Sigiliao <nicksigiliao@hotmail.com>
2021-11-13 16:31:54 -0300

    Pagina de Login usando Reactive Forms e ERRO no logout

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Usuario } from '../models/usuario.model';
-
+import { FormBuilder,FormGroup, Validators } from '@angular/forms';
 import { AuthLoginService } from './../auth/auth-login.service';
 import { UserService } from './../services/usuarios.service';
 
@@ -12,19 +12,46 @@ import { UserService } from './../services/usuarios.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+
   public email: string
-  public password: string
+  public senha: string
+  public userForm: FormGroup;
   public loggedUser: Usuario
 
   private sub: Subscription
 
-  constructor(private auth: AuthLoginService, private route: Router, private usuarioService: UserService) { }
+  constructor(private auth: AuthLoginService, private route: Router, private usuarioService: UserService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.userForm = this.formBuilder.group({
+      email: ['', Validators.compose([
+                  Validators.required,
+                  Validators.email])
+              ],
+      senha: ['', Validators.compose([
+                  Validators.required,
+                  Validators.minLength(6)])
+              ]
+    })
   }
 
+  
   public acessAccount() {
-    this.auth.logIn(this.email, this.password).then((response) => {
+    let user: Usuario = null;
+
+    if(this.userForm.valid){
+       
+      user = {
+        id: '',
+        nome: this.userForm.value.nomeCompleto,
+        nickName: this.userForm.value.nomeUsuario,
+        email: this.userForm.value.email,
+        senha: this.userForm.value.senha,
+        uid: ''
+      };
+    }
+    
+    this.auth.logIn(user.email, user.senha).then((response) => {
 
       if (response.user.uid) {
         this.sub = this.usuarioService.getByUID(response.user.uid).subscribe((users: Usuario[]) => {
@@ -39,11 +66,12 @@ export class LoginPage implements OnInit {
   }
 
   public logout() {
+    
     this.auth.logOut().then(() => {
       this.sub.unsubscribe()
       this.auth.setAuthenticated(false)
       this.email = ''
-      this.password = ''
+      this.senha = ''
     })
   }
 
